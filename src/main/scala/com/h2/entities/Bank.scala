35dbc7482bfd5cec901832368df2e99a23e589e2
franklin <forsonfranklin@gmail.com>
2023-01-14 12:54:06 -0500

    added the use credit card function

--- src/main/scala/com/h2/entities/Bank.scala
+++ src/main/scala/com/h2/entities/Bank.scala
@@ -13,24 +13,27 @@ class Bank (val name: String, val city: String , val country: String,val email: 
   private var customers: Map[UUID, Customer] = Map.empty
 
   println(s"$name Established 2018.")
-def createNewCustomer(first: String, last: String,
-                      email: String, dateOfBirth: String): UUID = {
 
-  def getEmail: Email = {
-    val Array(value, domain) = email.split("@")
-    Email(value,domain)
+  def createNewCustomer(first: String, last: String,
+                        email: String, dateOfBirth: String): UUID = {
+
+    def getEmail: Email = {
+      val Array(value, domain) = email.split("@")
+      Email(value, domain)
+    }
+
+    def getDateOfBirth: LocalDate = {
+      val Array(year, month, day) = dateOfBirth.split("/")
+      LocalDate.of(year.toInt, month.toInt, day.toInt)
+    }
+
+    val customer = new Customer(first, last, getEmail, getDateOfBirth)
+    customers += (customer.id -> customer)
+    customer.id
   }
 
-  def getDateOfBirth: LocalDate = {
-    val Array(year, month, day) = dateOfBirth.split("/")
-    LocalDate.of(year.toInt, month.toInt, day.toInt)
-  }
-  val customer = new Customer(first, last, getEmail, getDateOfBirth)
-  customers += (customer.id -> customer)
-  customer.id
-}
-  def addNewDepositProduct(name:String, minBalance:Int, ratePerYear:Double,
-                           transactionAllowedPerMonth: Int = 2 ) : UUID   = {
+  def addNewDepositProduct(name: String, minBalance: Int, ratePerYear: Double,
+                           transactionAllowedPerMonth: Int = 2): UUID = {
     val product = name match {
       case "CoreChecking" => new CoreChecking(Dollars(minBalance), ratePerYear)
       case "StudentCheckings" => new StudentCheckings(Dollars(minBalance), ratePerYear)
@@ -39,41 +42,44 @@ def createNewCustomer(first: String, last: String,
     depositProducts += (product.id -> product)
     product.id
   }
-  def addNewLendingProduct(annualFee: Double, apr:Double, rewardPercent: Double): UUID ={
+
+  def addNewLendingProduct(annualFee: Double, apr: Double, rewardPercent: Double): UUID = {
     val product = new CreditCard(annualFee, apr, rewardPercent)
     lendingProducts += (product.id -> product)
     product.id
   }
 
-def openDepositAccount(customerId: UUID, productId: UUID, amount: Dollars): UUID = {
-  require(customers.contains(customerId), s"no customer found with id=$customerId")
-  require(depositProducts.contains(productId) , s"no deposits product found with id=$productId")
-  val account = new DepositsAccount(customers(customerId),depositProducts(productId), amount)
-  depositAccounts += (account.id -> account)
-  account.id
-}
-def openLendingAccount(customerId: UUID, productId: UUID, balance:Dollars = Dollars(0)) : UUID = {
-  require(customers.contains(customerId), s"no customer found with id=$customerId")
-  require(lendingProducts.contains(productId), s"no lending product found with id=$productId")
- val account = new LendingAccount(customers(customerId), lendingProducts(productId), balance)
- lendingAccounts += (account.id -> account)
-  account.id
-}
+  def openDepositAccount(customerId: UUID, productId: UUID, amount: Dollars): UUID = {
+    require(customers.contains(customerId), s"no customer found with id=$customerId")
+    require(depositProducts.contains(productId), s"no deposits product found with id=$productId")
+    val account = new DepositsAccount(customers(customerId), depositProducts(productId), amount)
+    depositAccounts += (account.id -> account)
+    account.id
+  }
+
+  def openLendingAccount(customerId: UUID, productId: UUID, balance: Dollars = Dollars(0)): UUID = {
+    require(customers.contains(customerId), s"no customer found with id=$customerId")
+    require(lendingProducts.contains(productId), s"no lending product found with id=$productId")
+    val account = new LendingAccount(customers(customerId), lendingProducts(productId), balance)
+    lendingAccounts += (account.id -> account)
+    account.id
+  }
+
   //deposit mone
-  def deposit(accountId: UUID, amount: Dollars) : Unit ={
-    require(depositAccounts.contains(accountId),"A valid deposits account Id must be provided")
+  def deposit(accountId: UUID, amount: Dollars): Unit = {
+    require(depositAccounts.contains(accountId), "A valid deposits account Id must be provided")
     depositAccounts(accountId) deposit amount
   }
 
-
-
-
-
-
-
-
-  def withdraw(accountId:UUID, dollars: Dollars) : Unit ={
+  def withdraw(accountId: UUID, dollars: Dollars): Unit = {
     require(depositAccounts.contains(accountId), "A valid deposits account Id must be provided")
     depositAccounts(accountId) withdraw dollars
   }
+
+
+  def useCreditCard(accountId: UUID, dollars: Dollars): Unit = {
+    require(lendingAccounts.contains(accountId), "A valid lending account Id must be provided")
+    lendingAccounts(accountId) withdraw dollars
+  }
+
 }
