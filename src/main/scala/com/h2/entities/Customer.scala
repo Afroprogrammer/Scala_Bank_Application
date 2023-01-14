8161ddcdffd94732d58490f9ae6fb6b21835121f
franklin <forsonfranklin@gmail.com>
2023-01-14 12:23:31 -0500

    fixed a bug

--- src/main/scala/com/h2/entities/Customer.scala
+++ src/main/scala/com/h2/entities/Customer.scala
@@ -3,7 +3,7 @@ package main.scala.com.h2.entities
 import java.time.LocalDate
 import java.util.UUID
 
-class Customer(val first: String, val last: String ,  val email: String, val dateOfBirth: LocalDate) {
+class Customer(val first: String, val last: String ,  val email: Email, val dateOfBirth: LocalDate) {
   val id: UUID = UUID.randomUUID()
 
   override def toString: String = s"$id -> $first,$last,$email"
