f7bd26a3c64d6ac59b22a6db27ee8d4632799477
franklin <forsonfranklin@gmail.com>
2023-01-16 11:53:51 -0500

    created the account service

--- src/main/scala/com/h2/services/CustomerService.scala
+++ src/main/scala/com/h2/services/CustomerService.scala
@@ -1,5 +1,25 @@
 package main.scala.com.h2.services
 
-class CustomerService {
+import main.scala.com.h2.entities.{Customer, Email}
+
+import java.time.LocalDate
+import java.util.UUID
+
+trait CustomerService extends CustomerDb{
+
+  def createNewCustomer(first: String, last:String, email: String,dateOfBirth:String): UUID = {
+    def getEmail: Email = {
+      val Array(value, domain) = email.split("@")
+      Email(value,domain)
+    }
+    def getDateOfBirth: LocalDate = {
+      val Array(year, month, day) = dateOfBirth.split("/")
+      LocalDate.of(year.toInt, month.toInt, day.toInt)
+    }
+
+    val customer = new Customer(first, last, getEmail, getDateOfBirth)
+    saveCustomer(customer)
+    customer.id
+  }
 
 }
